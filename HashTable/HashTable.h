c5f1470ba476df4259b1eedf30f977715db9e29a
Jeremy <jswilde@g.clemson.edu>
2015-10-30 16:48:18 -0400

    update

--- HashTable/HashTable.h
+++ HashTable/HashTable.h
@@ -4,8 +4,7 @@
 #include <cstdlib>
 #include <iostream>
 
-	// This is a generic node type
-	// It takes a pointer to whatever legal data you send it
+// Not actually a hash table yet..
 
 template <class T>
 struct Node{
@@ -13,32 +12,3 @@ struct Node{
 	Node<T> *next;
 	T *data;
 };
-
-
-template <class T>
-class linkedList {
-
-	private:
-	
-	public:
-		Node<T> *head;
-		Node<T> *tail;
-		linkedList();
-		void insert( T* );
-		void removeNode( T* );
-		int size( );
-};
-
-
-template <class T>
-int linkedList<T>::size( ){
-
-	int count = 0;
-	struct Node<T> *curr = head;	
-	
-	while( curr != NULL ){
-		count++;
-		curr = curr->next;
-	}
-	return count;
-}
