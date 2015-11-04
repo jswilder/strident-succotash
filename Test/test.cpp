d613e88814bf9b7327b5f201c77aad9dc2256eec
Jeremy <ymerejeremy.jw@gmail.com>
2015-11-04 11:35:47 -0500

    Recursion test

---
+++ Test/test.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+	// Ret should = 1 for the base case
+void factorial( int &fact, int n ){
+
+	if( n == 0 ){
+		fact = 1;
+	}
+	else{
+		fact = fact * n;
+		n--;
+		if( n > 1 )
+			factorial( fact, n );
+	}
+}
+
+int main(){
+
+	int n = 4;
+	int ret = 1;
+	
+	factorial( ret, n );
+
+	std::cout << "\nfactorial is " << ret << std::endl;
+}
