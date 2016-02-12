34ad28c9931331a4e74ff667cbbe0b00e197443b
jswilder <jeremy.w.wilder@gmail.com>
2016-02-11 19:10:24 -0500

    Added Sundaram primes Impl

--- Test/test.cpp
+++ Test/test.cpp
@@ -4,7 +4,32 @@
 
 // Random Comment ?!?!
 
+class Shape{
+public:
+	std::string name;
+	Shape() : name("unnamed shape") {}
+	virtual void printStuff(){ std::cout << name << std::endl; };
+};
 
+class Circle : public Shape{
+
+public:
+	std::string name;
+	Circle() : name("Unnamed circle") {}
+	Circle(std::string n) : name(n) {}
+	void printStuff(){	std::cout<< name << std::endl;	}
+};
+
+class Dounut : public Circle{
+
+public:
+	std::string name;
+	Dounut() : name("Unnamed dounut") {}
+	Dounut(std::string n) : name(n) {}
+	void printStuff(){	std::cout << name << std::endl;	}
+};
+
+/*
 	// Ret should = 1 for the base case
 void factorial( int &fact, int n ){
 
@@ -18,13 +43,20 @@ void factorial( int &fact, int n ){
 			factorial( fact, n );
 	}
 }
-
+*/
 int main(){
 
-	int n = 4;
-	int ret = 1;
-	
-	factorial( ret, n );
+//	int n = 4;
+//	int ret = 1;
 
-	std::cout << "\nfactorial is " << ret << std::endl;
+	Shape *s = new Shape();
+	Shape *t = new Dounut();
+	Circle *u = new Dounut("Joe");
+
+	s->printStuff();
+	t->printStuff();
+	u->printStuff();
+
+//	factorial( ret, n );
+//	std::cout << "\nfactorial is " << ret << std::endl;
 }
